35662d8d30b6458e446b9e3fe8e2ece4b3314937
wk645 <wk645@nyu.edu>
2017-06-12 19:34:27 -0400

    badge lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+    badge_batch = []
+    
+    array_of_names.each do |name|
+        badge_batch << badge_maker(name)
+        end
+    badge_batch
+end
+
+def assign_rooms(speakers)
+    room_assignments = []
+    
+    speakers.each_with_index do |speakers, index|
+        room_assignments << "Hello, #{speakers}! You'll be assigned to room #{index + 1}!"
+        end
+    room_assignments
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
